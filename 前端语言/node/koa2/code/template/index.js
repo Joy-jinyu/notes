fe3d14e6c7ef4d4ac4630f0c75f4760d2153b40d
joy.yj.wang <joy.yj.wang@teleone.cn>
2020-09-30 16:55:15 +0800

    添加koa的代码

---
+++ 前端语言/node/koa2/code/template/index.js
@@ -0,0 +1,25 @@
+const Koa = require('koa'), 
+KoaParser = require('koa-bodyparser'),
+createController = require('./controllers/index')
+
+const app = new Koa(), 
+parser = KoaParser(),
+routes = createController()
+
+/**
+ * 中间件的演示，可忽略
+ */
+app.use(async (ctx, next) => {
+    /**
+     * 为什么要调用await next()？
+     * 原因是koa把很多async函数组成一个处理链，每个async函数都可以做一些自己的事情，然后用await next()来调用下一个async函数。我们把每个async函数称为middleware，这些middleware可以组合起来，完成很多有用的功能。
+     */
+    await next()
+})
+
+
+app.use(parser)
+app.use(routes)
+
+app.listen(3000)
+console.log('program has start on 3000')
